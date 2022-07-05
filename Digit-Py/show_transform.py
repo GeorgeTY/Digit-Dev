658d4b8bfe7bcd439431c870070581970abd606d
Zejun Yu <snda2008@live.cn>
2022-07-05 22:57:40 +0800

    Add new res & video rec, add temp impl

--- Digit-Py/show_transform.py
+++ Digit-Py/show_transform.py
@@ -1,10 +1,15 @@
 import sys
+from turtle import dot
 import cv2
 import time
 import numpy as np
 import digit_interface as Digit
 import matplotlib.pyplot as plt
 from pycpd import AffineRegistration
+from torch import argmin
+
+# ifVGA = True
+ifVGA = False
 
 
 def connectDigit(intensity=8):
@@ -15,13 +20,18 @@ def connectDigit(intensity=8):
     digit = Digit.Digit(digits[0]["serial"])
     digit.connect()
     digit.set_intensity(intensity)
+    if ifVGA:
+        digit.set_resolution(digit.STREAMS["VGA"])
     return digit
 
 
-def setVideoEncoder():
+def setVideoEncoder(scale=2):
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
     videoOut = cv2.VideoWriter()
-    videoOut.open("output.mp4", fourcc, 30, (640, 480))
+    if ifVGA:
+        videoOut.open("output.mp4", fourcc, 30, (480 * scale, 640 * scale))
+    else:
+        videoOut.open("output.mp4", fourcc, 30, (240 * scale, 320 * scale))
     return videoOut
 
 
@@ -31,18 +41,23 @@ def setDetectionParams():
     blobParams.minThreshold = 0
     blobParams.maxThreshold = 255
 
-    blobParams.filterByArea = True
-    blobParams.minArea = 10
-    blobParams.maxArea = 1000
+    if ifVGA:
+        blobParams.filterByArea = True
+        blobParams.minArea = 50
+        blobParams.maxArea = 500
+    else:
+        blobParams.filterByArea = True
+        blobParams.minArea = 20
+        blobParams.maxArea = 100
 
     blobParams.filterByCircularity = True
-    blobParams.minCircularity = 0.8
+    blobParams.minCircularity = 0.7
 
-    blobParams.filterByConvexity = True
-    blobParams.minConvexity = 0.8
+    # blobParams.filterByConvexity = True
+    # blobParams.minConvexity = 0.8
 
-    blobParams.filterByInertia = True
-    blobParams.minInertiaRatio = 0.01
+    # blobParams.filterByInertia = True
+    # blobParams.minInertiaRatio = 0.01
 
     return blobParams
 
@@ -76,25 +91,61 @@ def dotDetection(blobDetector, Frm, circleColor=(0, 0, 255)):
     return keypoints, Frm_with_keypoints
 
 
-def dotMatching(X, Y):
+def dotRegistration(keypoints_a, keypoints_b):
+    X = np.array([keypoint.pt for keypoint in keypoints_a])
+    Y = np.array([keypoint.pt for keypoint in keypoints_b])
+    TY, ((s, R), t) = AffineRegistration(
+        **{"X": X, "Y": Y}
+    ).register()  ## CPD registration
+
+    return X, Y, TY, s, R, t
+
+
+def dotMatching(X, Y, TY, Frm0, Frm, scale=2):
+    distance, dotPair = np.zeros((len(X), len(Y))), np.zeros((len(X), len(Y)))
+
+    Frm_dot_movement = cv2.addWeighted(Frm, 0.65, Frm0, 0.35, 0)
+    Frm_dot_movement = cv2.resize(
+        Frm_dot_movement,
+        (scale * Frm_dot_movement.shape[1], scale * Frm_dot_movement.shape[0]),
+        interpolation=cv2.INTER_AREA,
+    )
+
     for i in range(len(X)):
-        for j in range(len(Y)):
-            if np.linalg.norm(X[i] - Y[j]) < 10:
-                return True
-    return
+        for j in range(len(TY)):
+            distance[i][j] = np.linalg.norm(X[i] - TY[j])
+        argmin_i = np.argmin(distance[i])
+
+        ## Temporary Implementation: Prevent the dot from being registered twice
+        while dotPair[i][argmin_i] == 1:
+            distance[i][argmin_i] = np.inf
+            argmin_i = np.argmin(distance[i])
+
+        dotPair[i][argmin_i] = 1
+        cv2.arrowedLine(
+            Frm_dot_movement,
+            (int(X[i][0]) * scale, int(X[i][1]) * scale),
+            (
+                int(Y[argmin_i][0] * scale),
+                int(Y[argmin_i][1]) * scale,
+            ),
+            (0, 0, 255),
+            2,
+        )
+    return Frm_dot_movement, dotPair
 
 
 def main():
     digit = connectDigit()
-
     for _ in range(30):  # Preheat the digit
         Frm0 = digit.get_frame()
 
-    blobDetector = cv2.SimpleBlobDetector_create(setDetectionParams())
     videoOut = setVideoEncoder()
     print(
         "Press ESC to quit, O to capture Original, C to capture Difference, D to show Difference."
     )
+
+    blobDetector = cv2.SimpleBlobDetector_create(setDetectionParams())
 
     while True:
         Frm = digit.get_frame()
@@ -102,17 +153,21 @@ def main():
         ## Dot detection
         keypoints, Frm_with_keypoints = dotDetection(blobDetector, Frm)
 
-        cv2.imshow("Keypoints", Frm_with_keypoints)
-        cv2.moveWindow("Keypoints", 100, 100)
+        cv2.imshow("Preview", Frm_with_keypoints)
+        cv2.moveWindow("Preview", 100, 100)
 
         getKey = cv2.waitKey(1)
-        if getKey == 27:  # ESC
+        if getKey == 27 or getKey == ord("q"):  # ESC or q
             break
+        if len(keypoints) == 0:  # No dot detected cause error
+            continue
         elif getKey == ord("o"):  # Get original frame
             Frm_a = Frm
             keypoints_a = keypoints
-            X = np.array([keypoint.pt for keypoint in keypoints_a])
+            Frm_a_with_keypoints = Frm_with_keypoints
             print("Original Frame Got.")
+            cv2.imshow("Original", Frm_a_with_keypoints)
+            cv2.moveWindow("Original", 100, 550)
         elif getKey == ord("c"):  # Capture difference
             digit.show_view(digit.get_frame())
             cv2.waitKey(0)
@@ -120,25 +175,32 @@ def main():
         elif getKey == ord("d"):  # Show difference
             while True:
                 Frm = digit.get_frame()
-                keypoints_b, Frm_b = dotDetection(blobDetector, Frm)
-                Y = np.array([keypoint.pt for keypoint in keypoints_b])
-                TY, ((s, R), t) = AffineRegistration(
-                    **{"X": X, "Y": Y}
-                ).register()  ## CPD registration
-                print("TY: ", TY)
-                print("s: ", s)
-                print("R: ", R)
-                print("t: ", t)
+                Frm_b = Frm
+                keypoints_b, Frm_b_with_keypoints = dotDetection(blobDetector, Frm)
 
-                cv2.imshow("Difference", Frm_with_keypoints)
+                ## Temporary implementation
+                if len(keypoints_a) != len(keypoints_b):
+                    continue
+
+                X, Y, TY, s, R, t = dotRegistration(keypoints_a, keypoints_b)
+                Frm_dot_movement, dotPair = dotMatching(X, Y, TY, Frm_a, Frm_b)
+
+                videoOut.write(Frm_dot_movement)
+
+                cv2.destroyWindow("Preview")
+                cv2.moveWindow("Original", 100, 100)
+                cv2.imshow("Dot Movement", Frm_dot_movement)
+                cv2.moveWindow("Dot Movement", 490, 100)
+                cv2.imshow("Current", Frm_b_with_keypoints)
+                cv2.moveWindow("Current", 100, 550)
                 getKey = cv2.waitKey(1)
-                if getKey == 27:  # ESC
+                if getKey == 27 or getKey == ord("q"):  # ESC or q
                     break
-                cv2.moveWindow("Difference", 100, 500)
             break
 
     ## Turn off the digit
     videoOut.release()
+    print("Video saved to ./output.mp4")
     digit.disconnect()
 
 
--- Digit-Py/track_markers.py
+++ Digit-Py/track_markers.py
@@ -4,26 +4,7 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 from pycpd import AffineRegistration
-from show_transform import dotDetection
-
-
-def dotMatching(X, Y, TY, Frm):
-    distance = np.zeros((len(X), len(Y)))
-    dotPair = np.zeros((len(X), len(Y)))
-    for i in range(len(X)):
-        for j in range(len(TY)):
-            distance[i][j] = np.linalg.norm(X[i] - TY[j])
-        argmin_i = np.argmin(distance[i])
-        print(np.min(distance[argmin_i]), ":", argmin_i)
-        dotPair[i][argmin_i] = 1
-        cv2.line(
-            Frm,
-            (int(X[i][0]), int(X[i][1])),
-            (int(Y[np.argmin(distance[i])][0]), int(Y[np.argmin(distance[i])][1])),
-            (0, 0, 255),
-            2,
-        )
-    return Frm, dotPair
+from show_transform import dotDetection, dotMatching, dotRegistration
 
 
 tic = time.time()
@@ -38,10 +19,10 @@ Y = np.array([keypoint.pt for keypoint in keypoints_Frm0])
 
 reg = AffineRegistration(**{"X": X, "Y": Y})
 TY, ((s, R), t) = reg.register()
-dotMovement, dotPair = dotMatching(X, Y, TY, Frm0)
+
+Frm_dot_movement, dotPair = dotMatching(X, Y, TY, Frm0, Frm)
 print("dotPair: \n", dotPair)
-dotMovement = cv2.addWeighted(Frm, 0.5, dotMovement, 0.5, 0)
-cv2.imshow("Dot Movement", dotMovement)
+cv2.imshow("Dot Movement", Frm_dot_movement)
 cv2.moveWindow("Dot Movement", 100, 100)
 
 toc = time.time()
