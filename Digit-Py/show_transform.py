da9995f48f391b8110ef9d35479a18faa1420a4f
Zejun Yu <snda2008@live.cn>
2022-07-08 16:38:32 +0800

    subdivided functions

--- Digit-Py/show_transform.py
+++ Digit-Py/show_transform.py
@@ -1,25 +1,13 @@
-import sys
 import cv2
 import time
 import numpy as np
 from global_params import *
-import digit_interface as Digit
 import matplotlib.pyplot as plt
-from pycpd import DeformableRegistration
+from track_deform import dotSegment
+from connect_digit import connectDigit
 from genetic_calc import calcMatrixM
-
-
-def connectDigit(intensity=8):
-    digits = Digit.DigitHandler.list_digits()
-    if len(digits) == 0:
-        sys.exit("No Digit Found uwu")
-
-    digit = Digit.Digit(digits[0]["serial"])
-    digit.connect()
-    digit.set_intensity(intensity)
-    if ifVGA:
-        digit.set_resolution(digit.STREAMS["VGA"])
-    return digit
+from detect_blob import setDetectionParams, dotDetection
+from track_markers import dotRegistration, dotMatching
 
 
 def setVideoEncoder(scale=2):
@@ -41,168 +29,6 @@ def setVideoEncoder(scale=2):
             (240 * scale, 320 * scale),
         )
     return videoOut
-
-
-def setDetectionParams():
-    blobParams = cv2.SimpleBlobDetector_Params()
-
-    blobParams.minThreshold = 0
-    blobParams.maxThreshold = 255
-
-    if ifVGA:
-        blobParams.filterByArea = True
-        blobParams.minArea = 50
-        blobParams.maxArea = 500
-    else:
-        blobParams.filterByArea = True
-        blobParams.minArea = 20
-        blobParams.maxArea = 100
-
-    blobParams.filterByCircularity = True
-    blobParams.minCircularity = 0.7
-
-    # blobParams.filterByConvexity = True
-    # blobParams.minConvexity = 0.8
-
-    # blobParams.filterByInertia = True
-    # blobParams.minInertiaRatio = 0.01
-
-    return blobParams
-
-
-def dotDetection(blobDetector, Frm, circleColor=(0, 0, 255)):
-    keypoints = blobDetector.detect(Frm)
-    Frm_with_keypoints = cv2.drawKeypoints(
-        Frm,
-        keypoints,
-        np.array([]),
-        (50, 50, 150),
-        cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS,
-    )
-    for keypoint in keypoints:
-        cv2.circle(
-            Frm_with_keypoints,
-            (int(keypoint.pt[0]), int(keypoint.pt[1])),
-            1,
-            circleColor,
-            -1,
-            cv2.LINE_AA,
-        )
-    Frm_with_keypoints = cv2.putText(
-        Frm_with_keypoints,
-        "{}".format(len(keypoints)),
-        (5, 315),
-        cv2.FONT_HERSHEY_COMPLEX,
-        0.5,
-        (255, 255, 255),
-        1,
-        cv2.LINE_AA,
-    )
-    return keypoints, Frm_with_keypoints
-
-
-def dotRegistration(keypoints_a, keypoints_b):
-    X = np.array([keypoint.pt for keypoint in keypoints_a])
-    Y = np.array([keypoint.pt for keypoint in keypoints_b])
-
-    TY, (G, W, P) = DeformableRegistration(
-        **{"X": X, "Y": Y}, alpha=alpha, beta=beta
-    ).register()  ## CPD registration
-
-    return X, Y, TY, G, W, P
-
-
-def getRegParam(self):
-    return self.G, self.W, self.P
-
-
-def dotMatching(X, Y, TY, P, Frm0, Frm, scale=2):
-    Frm_dot_movement = cv2.addWeighted(Frm, 0.65, Frm0, 0.35, 0)
-    Frm_dot_movement = cv2.resize(
-        Frm_dot_movement,
-        (scale * Frm_dot_movement.shape[1], scale * Frm_dot_movement.shape[0]),
-        interpolation=cv2.INTER_AREA,
-    )
-
-    #### Dot matching using P ####
-    dotPair = np.zeros_like(P)
-    for i in range(np.shape(P)[0]):
-        dotPair[i][np.argmax(P[i][:])] = 1
-
-    # dotPair = np.zeros_like(P)
-    # while True:
-    #     continueFlag = False
-    #     for i in range(np.shape(P)[0]):
-    #         argmax_j = np.argmax(P[i][:])
-    #         # dotPairProb[i][argmax_j] = P[i][argmax_j]
-    #         if np.count_nonzero(dotPair[:][argmax_j]) > 0:
-    #             j = argmax_j
-    #             argmax_i = np.argmax(dotPair[:][argmax_j])
-    #             if P[i][j] > dotPair[argmax_i][j]:
-    #                 dotPair[i][j] = P[i][j]
-    #                 dotPair[argmax_i][j] = 0
-    #                 P[argmax_i][j] = 0
-    #             else:
-    #                 P[i][j] = 0
-    #         else:
-    #             dotPair[i][argmax_j] = P[i][argmax_j]
-
-    #     for i in range(np.shape(P)[0]):
-    #         if np.count_nonzero(dotPair[i][:]) > 1:
-    #             continueFlag = True
-    #             break
-    #     for j in range(np.shape(P)[1]):
-    #         if np.count_nonzero(dotPair[:][j]) > 1:
-    #             continueFlag = True
-    #             break
-    #     if not continueFlag:
-    #         break
-    #### End of Dot matching using P ####
-
-    #### Dot matching using TY distance ####
-    # distance = np.zeros((len(X), len(TY)))
-    # dotPair = np.zeros((len(X), len(TY))).astype(bool)
-
-    # for i in range(len(X)):
-    #     for j in range(len(TY)):
-    #         distance[i][j] = np.linalg.norm(X[i] - TY[j])
-    #     argmin_j = np.argmin(distance[i][:])
-
-    #     # ## Temporary Implementation: Prevent the dot from being registered twice
-    #     # while dotPair[i][argmin_j] == 1:
-    #     #     distance[i][argmin_j] = np.inf
-    #     #     argmin_j = np.argmin(distance[i][:])
-    #     # ## Temporary Implementation end
-
-    #     dotPair[i][argmin_j] = 1
-    #### End of Dot matching using TY distance ####
-
-    #### Dot matching using P . dotPair ####
-    # dotPair = calcMatrixM(P) ## Too slow
-    #### End of Dot matching using P . dotPair ####
-
-    np.savetxt("output/saved_dotPair.out", dotPair, delimiter=",")
-    for i in range(np.shape(P)[0]):
-        for j in range(np.shape(P)[1]):
-            if dotPair[i][j] > 0:
-                cv2.arrowedLine(
-                    Frm_dot_movement,
-                    (int(X[j][0]) * scale, int(X[j][1]) * scale),
-                    (
-                        int(Y[i][0] * scale),
-                        int(Y[i][1]) * scale,
-                    ),
-                    (0, 0, 255),
-                    2,
-                    cv2.LINE_AA,
-                )
-                break
-
-    return Frm_dot_movement, dotPair
-
-
-## Add P output to existing function
-DeformableRegistration.get_registration_parameters = getRegParam
 
 
 def main():
@@ -256,12 +82,17 @@ def main():
                 X, Y, TY, G, W, P = dotRegistration(keypoints_a, keypoints_b)
                 Frm_dot_movement, dotPair = dotMatching(X, Y, TY, P, Frm_a, Frm_b)
 
-                videoOut.write(Frm_dot_movement)
+                tri, Frm_dot_segment = dotSegment(Y, Frm_dot_movement, 2, (0, 255, 255))
+                tri, Frm_dot_segment = dotSegment(X, Frm_dot_segment, 2, (255, 255, 0))
+
+                videoOut.write(Frm_dot_segment)
 
                 cv2.destroyWindow("Preview")
                 cv2.moveWindow("Original", 100, 100)
                 cv2.imshow("Dot Movement", Frm_dot_movement)
                 cv2.moveWindow("Dot Movement", 490, 100)
+                cv2.imshow("Dot Segment", Frm_dot_segment)
+                cv2.moveWindow("Dot Segment", 490, 550)
                 cv2.imshow("Current", Frm_b_with_keypoints)
                 cv2.moveWindow("Current", 100, 550)
                 getKey = cv2.waitKey(1)
